6495f61fc030c24fa1da82508ed25fb6cc94d182
davidsgrz <davidsegura97@gmail.com>
2025-11-05 12:15:52 +0100

    errata

--- dockerfiles/base/admin/start.sh
+++ dockerfiles/base/admin/start.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+
+
+newuser() {
+        useradd -rm -d / home/david - s/bin/bash david
+    
+    echo "david:1234" | chpasswd
+    echo "BIENVENIDO DAVID..." >> /home/david/BIENVENIDA_DAVID.txt
+}
+
+main() {
+    newuser
 #Encargada de mantener el contenedor en ejecución
 tail -f /dev/null
-
+}
+main
+   
