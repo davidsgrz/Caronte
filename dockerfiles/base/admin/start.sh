50bd75a206f64a1bfb76fccca8fa3404f6537557
davidsgrz <davidsegura97@gmail.com>
2025-11-11 12:47:37 +0100

    devop arg env

--- dockerfiles/base/admin/start.sh
+++ dockerfiles/base/admin/start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
+#carga las variables de entronno desde el docker compose
+set -e
 
 
 newUser() {
         useradd -rm -d /home/${USUARIO} -s /bin/bash david
-    
-    echo "${USUARIO}:1234" | chpasswd
-    echo "BIENVENIDO ${USUARIO}..." >> /home/${USUARIO}/BIENVENIDA_DAVID.txt
+        echo "${USUARIO}:1234" | chpasswd
+        echo "BIENVENIDO ${USUARIO}..." >> /home/${USUARIO}/BIENVENIDA_DAVID.txt
 }
 
 main() {
