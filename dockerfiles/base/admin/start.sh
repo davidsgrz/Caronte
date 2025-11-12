62db14b8e5e78379b971c4fea47b7bd15ec7e119
davidsgrz <davidsegura97@gmail.com>
2025-11-12 10:45:52 +0100

    script

--- dockerfiles/base/admin/start.sh
+++ dockerfiles/base/admin/start.sh
@@ -2,28 +2,48 @@
 #carga las variables de entronno desde el docker compose
 set -e
 
-check_usuario (){
+check_usuario() {
     if grep -q "${USUARIO}" /etc/passwd
     then
-        echo "El usuario ${USUARIO} existe" >> /root/logs/informe.log
-        return 1
+                echo "El usuario ${USUARIO} existe" >> /root/logs/informe.log
+                return 1
     else
-        echo "El usuario ${USUARIO} no existe" >> /root/logs/informe.log
-        return 0
+                echo "El usuario ${USUARIO} no existe" >> /root/logs/informe.log
+                return 0
     fi
+}
+check_home() {
+        if [ ! -d "/home/${USUARIO}" ]
+        then
+                echo "/home/${USUARIO} no existe" >> /root/logs/informe.log
+                return 0
+        else
+                echo "/home/${USUARIO} existe" >> /root/logs/informe.log
+                return 1
+        fi
 }
 
 newUser() {
         check_usuario
         if [ "$?" -eq 0 ]
         then
-            useradd -rm -d "/home/${USUARIO}" -s /bin/bash "${USUARIO}"
-            echo "${USUARIO}:${PASSWORD}" | chpasswd
-            echo "BIENVENIDO ${USUARIO}..." >> /home/"${USUARIO}"/BIENVENIDA_DAVID.txt
+            check_home
+            if [ "$?" -eq 0 ]
+            then
+                    useradd -rm -d "/home/${USUARIO}" -s /bin/bash "${USUARIO}"
+                    echo "${USUARIO}:${PASSWORD}" | chpasswd
+                    echo "BIENVENIDO ${USUARIO}..." > /home/"${USUARIO}"/BIENVENIDA_DAVID.txt
+            else
+                    echo "${USUARIO} no creado,existe home" >> /root/logs/informe.log
+            fi
+            else
+                    echo "${USUARIO} no creado,existe en psswd" >> /root/logs/informe.log
         fi
 }
 
 main() {
+    mkdir -p /root/logs
+    touch /root/logs/informe.log
     newUser
 #Encargada de mantener el contenedor en ejecución y que no muera
     tail -f /dev/null
