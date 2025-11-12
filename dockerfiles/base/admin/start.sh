f9403144ab6b9c17dfa44b70a54e61bc06643022
davidsgrz <davidsegura97@gmail.com>
2025-11-12 12:08:27 +0100

    script llamada

--- dockerfiles/base/admin/start.sh
+++ dockerfiles/base/admin/start.sh
@@ -2,7 +2,7 @@
 # carga las variables de entono pasadas desde el D.Compose
 set -e
 
-#source /root/admin/base/usuarios/mainUsuarios.sh
+source /root/admin/base/usuarios/mainUsuarios.sh
 
 main() {
     # gestion usuario ---> getsUser.sh
@@ -11,7 +11,7 @@ main() {
     # ...
     mkdir -p /root/logs
     touch /root/logs/informe.log
-    #newUser
+    newUser
     # encargada de dejar este contendor vivo en BGround
     tail -f /dev/null
     ## script's que se encargar de configurar el imagen/contenedor
