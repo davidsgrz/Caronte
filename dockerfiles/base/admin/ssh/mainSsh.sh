8994e4884f7d64b8a778a26b94038453819d6fd6
davidsgrz <davidsegura97@gmail.com>
2025-11-19 10:36:09 +0100

    commi

--- dockerfiles/base/admin/ssh/mainSsh.sh
+++ dockerfiles/base/admin/ssh/mainSsh.sh
@@ -5,3 +5,7 @@ make_ssh(){
     sed -i 's/#Port 22/Port 2345/' /etc/ssh/sshd_config
     sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /
 }
+
+
+#etc/init.d/ssh start &
+#exec /usr/sbin/sshd -D &  #dejar ssh en background
