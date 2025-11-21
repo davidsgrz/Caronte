aae7c8efe0d02590df6dc65f78e6be60e3ddfb25
davidsgrz <davidsegura97@gmail.com>
2025-11-21 09:39:24 +0100

    dx9

--- dockerfiles/base/admin/ssh/mainSsh.sh
+++ dockerfiles/base/admin/ssh/mainSsh.sh
@@ -3,7 +3,7 @@ set -e
 make_ssh(){
     sed -i 's/#Port 22/Port '$PORT_SSH'/' /etc/ssh/sshd_config
     sed -i 's/#PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
-
+    service ssh restart
     mkdir -p /home/${USUARIO}/.ssh
    # cp /root/common/id_ed25519.pub /home/${USUARIO}/.ssh/
     cat /root/admin/base/common/id_rsa.pub >> /home/${USUARIO}/.ssh/authorized_keys
