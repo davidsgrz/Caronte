4631eb4fb90b6f731436bc49528dd0ebe0224c76
davidsgrz <davidsegura97@gmail.com>
2025-11-21 09:23:43 +0100

    dx6

--- dockerfiles/base/admin/start.sh
+++ dockerfiles/base/admin/start.sh
@@ -9,18 +9,15 @@ main() {
     # gestion del sudo ---> gestSudo.sh
     # gestion del ssh ---> gestSsh.sh
     # ...
-     touch /root/logs/informe.log
+    touch /root/logs/informe.log
     newUser
     resuser=$?
     if [ "$resuser" -eq 0 ]
     then
         make_ssh
-    fi
-
-    if [ "$?" -eq 0 ]
-    then
         make_sudo
     fi
+
     # encargada de dejar este contendor vivo en BGround
     tail -f /dev/null
     ## script's que se encargar de configurar el imagen/contenedor
