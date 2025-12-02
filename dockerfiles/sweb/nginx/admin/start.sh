60c273a81ef460047eb2f25826ab5e6f5e0d5e35
davidsgrz <davidsegura97@gmail.com>
2025-12-02 13:10:20 +0100

    fds

---
+++ dockerfiles/sweb/nginx/admin/start.sh
@@ -0,0 +1,15 @@
+config_nginx () {
+    nginx &
+}
+
+load_entrpoint_base () {
+
+    bash /root/admin/base/start.sh
+}
+
+main() {
+    load_entrpoint_base
+    config_nginx
+
+tail -f /dev/null
+}
