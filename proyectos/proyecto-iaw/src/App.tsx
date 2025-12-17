6b7c936e37aa69a6c56d39bccbece2d28c06f54e
davidsgrz <davidsegura97@gmail.com>
2025-12-17 09:22:18 +0100

    sfdvx

--- proyectos/proyecto-iaw/src/App.tsx
+++ proyectos/proyecto-iaw/src/App.tsx
@@ -1,22 +1,35 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Inicio from "./paginas/inicio";
-import Supermercados  from "./paginas/supermercados";
-import Trabaja from "./paginas/trabaja";
-import AtCliente from "./paginas/at_cliente";
-import Error404 from "./paginas/error404"; 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Header } from "./Components/Header";
+import { Navbar } from "./Components/navbar";
+import { CardPokemon } from "./Components/Poke";
+import { Digimon } from "./Components/Digimon";
+import { Error } from "./Components/Errores";
 
-function App() {
+export const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/supermercados" element={<Supermercados />} />
-        <Route path="/trabaja" element={<Trabaja />} />
-        <Route path="/at_cliente" element={<AtCliente />} />
-        <Route path="*" element={<Error404 />} />
-      </Routes>
-    </Router>
-  );
-}
+    <BrowserRouter>
 
-export default App
+      {/* CONTENEDOR CENTRAL DE TODA LA WEB */}
+      <div className="app-container">
+
+        <Header />
+        <Navbar />
+
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<CardPokemon min={1} max={151} />} />
+            <Route path="/gen1" element={<CardPokemon min={1} max={151} />} />
+            <Route path="/gen2" element={<CardPokemon min={152} max={251} />} />
+            <Route path="/gen3" element={<CardPokemon min={252} max={386} />} />
+            <Route path="/digimon" element={<Digimon />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </main>
+
+      </div>
+
+    </BrowserRouter>
+  );
+};
+
+export default App;
